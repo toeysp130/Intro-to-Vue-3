370c27b2f35987e3d7f249638d4338965e374789
toeysp130 <choptum@hotmail.co.th>
2021-04-07 10:11:49 +0700

    commit

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -50,13 +50,13 @@ app.component('product-display', {
         details: ['50% cotton', '30% wool', '20% polyester'],
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+          { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 20 },
         ]
     }
   },
   methods: {
         addToCart(){
-            this.$emit('add-to-cart')
+            this.$emit('add-to-cart',this.variants[this.selectedVariant].id)
         },
         updateVariant(index) {
             this.selectedVariant = index
